5cc6f6726d2581bc415d7d539655251ab88757b2
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-11-29 22:32:58 +0100

    .

---
+++ src/preprocessing/local_ci.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true
+do
+    docker-compose up -d jupyterds2
+    docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
+    docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/4_preprocess_for_simulation.ipynb
+    sleep 1200
+done
--- src/preprocessing/run_ci.sh
+++ src/preprocessing/run_ci.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker-compose up -d jupyterds2
 # DOWNLOAD TRAINDATA
-docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
+docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
 # DOWNLOAD COVID DATA
-docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/2_download_rki_data.ipynb
+docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/2_download_rki_data.ipynb
 # PREPROCESS DATA = TRAIN + COVID
-docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/4_preprocess_for_simulation.ipynb
+docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/4_preprocess_for_simulation.ipynb
 
 # PUSH RESULTS
 #docker-compose down jupyterds2
