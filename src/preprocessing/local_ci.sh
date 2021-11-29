781921e9c0468b06b7e09284d1e13529011c0db7
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-11-30 00:36:08 +0100

    .

--- src/preprocessing/local_ci.sh
+++ src/preprocessing/local_ci.sh
@@ -3,6 +3,7 @@ while true
 do
     docker-compose up -d jupyterds2
     docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
+    docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/2_download_rki_data.ipynb
     docker exec jupyterds2 jupyter nbconvert --inplace --execute --to notebook /home/jovyan/work/4_preprocess_for_simulation.ipynb
     sleep 1200
 done
