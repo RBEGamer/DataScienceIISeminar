7a9d00fd56ecdc85337445b88131d5de39478263
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-11-29 22:00:02 +0100

    added covid download to ci runner

--- src/preprocessing/run_ci.sh
+++ src/preprocessing/run_ci.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 docker-compose up -d jupyterds2
+# DOWNLOAD TRAINDATA
 docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
-docker-compose down jupyterds2
+# DOWNLOAD COVID DATA
+docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/2_download_rki_data.ipynb
+# PREPROCESS DATA = TRAIN + COVID
+docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/4_preprocess_for_simulation.ipynb
+
+# PUSH RESULTS
+#docker-compose down jupyterds2
 ls -la ./generated/departure_tables_raw
 git pull origin/main main
-
 git add ./generated/departure_tables_raw
