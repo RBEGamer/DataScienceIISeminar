cad3cce7ab22d4b565cf28c86b3885d717f35908
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-11-29 21:24:20 +0100

    .

--- src/preprocessing/run_ci.sh
+++ src/preprocessing/run_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose up -d
+docker-compose up -d jupyterds2
 docker exec jupyterds2 jupyter nbconvert --execute --to notebook /home/jovyan/work/1_download_train_schedule.ipynb
-docker-compose down
+docker-compose down jupyterds2
 ls -la ./generated/departure_tables_raw
 git add ./generated/departure_tables_raw
