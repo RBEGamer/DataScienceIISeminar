34597dffe1e5ff136f3fe7471797194e909178b7
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-11-30 14:58:19 +0100

    .

--- src/preprocessing/local_ci_no_docker.sh
+++ src/preprocessing/local_ci_no_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 while true
 do
+    git pull
     python3 ./1_download_train_schedule.py
     python3 ./2_download_rki_data.py
+    git add *
+    git commit -m "ci push"
+    git push
     sleep 1200
 done
