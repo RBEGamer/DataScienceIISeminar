358fc032041c33ae6452abf30a18492204c3ebc0
C I <cisyste@prodevmo.com>
2021-11-30 19:06:03 +0100

    CI

--- src/preprocessing/local_ci_no_docker.sh
+++ src/preprocessing/local_ci_no_docker.sh
@@ -4,7 +4,7 @@ do
     git pull
     python3 ./1_download_train_schedule.py
     python3 ./2_download_rki_data.py
-    git add *
+    git add generated/*.*
     git commit -m "ci push"
     git push
     sleep 1200
