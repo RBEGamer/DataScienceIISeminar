4a14aabaceec6032ebf137cc16e1c4774a0121af
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-12-01 21:32:57 +0100

    .

--- src/preprocessing/visualisation_server/server.js
+++ src/preprocessing/visualisation_server/server.js
@@ -7,9 +7,6 @@ var express = require('express');
 var session = require('express-session');//sessions
 var bodyParser = require('body-parser');//sessions
 var sessionstore = require('sessionstore'); //sessions
-var os = require("os");
-var chalk = require('chalk'); //colors in console
-var mqtt = require('mqtt'); //mqtt client
 var config = require('./config.json'); //include the cofnig file
 var uuidv1 = require('uuid/v1'); //gen random uuid times based
 var got = require('got');
@@ -20,14 +17,14 @@ var fileUpload = require('express-fileupload');
 var cron = require('node-cron'); //some cronjobs
 var listEndpoints = require('express-list-endpoints'); //for rest api explorer
 var bcrypt = require('bcrypt'); //for pw hash
-var DB = require('tingodb')().Db;//file based database like mongo db
+
 
 
 var port = process.env.PORT || config.webserver_default_port || 3016;
 var hostname = process.env.HOSTNAME || config.hostname || "http://127.0.0.1:" + port + "/";
 var appDirectory = require('path').dirname(process.pkg ? process.execPath : (require.main ? require.main.filename : process.argv[0]));
 console.log(appDirectory);
-var db = new DB(path.join(appDirectory, 'db'), {});
+
 
 
 
@@ -68,16 +65,6 @@ server.listen(port, function () {
 // ---------------- END EXPRESS SETUP -------------- //
 
 
-//--------- DB -----------------------------------//
-var collection = db.collection("doc");
-
-
-collection.insert([{ email: 'test@user.de', password: bcrypt.hashSync('test', 10), id: uuidv1(), type: "USER" }], function (err, result) { console.log(err); console.log(result); });
-
-collection.findOne({ email: 'test@user.de' }, function (err, item) {
-    console.log(err);
-});
-
 //-------- HELPER FUNCTIONS ---------------//
 function generate_random_string() {
     return randomstring.generate({
@@ -104,107 +91,7 @@ app.get('/index', function (req, res) {
 });
 
 
-app.get('/register', function (req, res) {
-    res.render('register.ejs', {
-        app_name: config.app_name
-    });
-});
-app.post('/register', function (req, res) {
-    sess = req.session;
-    var user_email = req.body.email;
-    var user_pw = req.body.password;
-    var user_name = req.body.name;
 
-    collection.findOne({ email: user_email }, function (_err, item) {
-        if (!_err || item) { res.redirect("/error?msg=email_registered"); return; }
-
-        collection.insert([{name:sanitizer.sanitize(user_name), email: sanitizer.sanitize(user_email), password: bcrypt.hashSync(sanitizer.sanitize(user_pw), 10), id: uuidv1(), type: "USER" }], function (err, result) { 
-            if (err) { res.redirect("/error?msg=register_db_write_failed"); return; }
-            res.redirect("/login");
-            return;
-        });
-    });
-});
-app.get('/login', function (req, res) {
-    res.render('login.ejs', {
-        app_name: config.app_name
-    });
-});
-
-app.post('/login', (req, res) => {
-    sess = req.session;
-    var user_email = req.body.email;
-    var user_pw = req.body.password;
-    collection.findOne({ email: user_email }, function (err, item) {
-        if (err) { res.redirect("/error?msg=login_failed"); return; }
-        bcrypt.compare(user_pw, item.password, function (err, crypr_res) {
-            if (crypr_res) {
-                sess.user_data = item;//ADD SESSION VARS
-                console.log(sess);
-                res.redirect("/admin");
-                return;
-            } else {
-                if (err) { res.redirect("/error?msg=login_failed"); return; }
-            }
-        });
-    });
-});
-
-app.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return console.log(err);
-        }
-        res.redirect('/');
-    });
-});
-
-
-
-
-
-app.get('/admin', (req, res) => {
-    sess = req.session;
-    if (sess.user_data) {
-        res.write(`<h1>Hello ${sess.user_data.email} </h1><br>`);
-        res.end('<a href=' + '/logout' + '>Logout</a>');
-    }
-    else {
-        res.write('<h1>Please login first.</h1>');
-        res.end('<a href=' + '/login' + '>Login</a>');
-    }
-});
-
-
-
-app.get('/error', function (req, res) {
-    res.render('error.ejs', {
-        err: sanitizer.sanitize(req.query.msg),
-        app_name: config.app_name
-    });
-});
-
-
-/*
-app.post('/create_project', function (req, res) {
-    if (req.body.project_name == undefined || req.body.project_desc == undefined || sanitizer.sanitize(req.body.project_name) == "" || sanitizer.sanitize(req.body.project_desc) == "") {
-        res.finished = true;
-        return;
-    }
-*/
-
-//uuidv1(); --- GEN UUID
-
-
-
-
-
-
-app.get('/rest/update/:id', function (req, res) {
-    var id = req.params.id;
-    var tmp = { "id": id };
-    res.json(tmp);
-});
 
 
 //---------------- SOCKET IO START ------------- //
